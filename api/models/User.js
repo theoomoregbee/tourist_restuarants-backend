0d6ab96cc3361e46dc62a73ee0b9f01ee219195e
Theophilus Omorebee <theo4u@ymail.com>
2017-06-24 01:54:17 +0100

    always encrypt password fields

--- api/models/User.js
+++ api/models/User.js
@@ -4,6 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var bcrypt = require("bcryptjs");
 
 module.exports = {
 
@@ -52,6 +53,41 @@ module.exports = {
         password: {
             required: 'Password is required'
         }
+    },
+
+    /**
+     * this is called so we can create our password hash for us
+     * before saving
+     * @param values
+     * @param cb
+     */
+    beforeCreate: function (values, cb) {
+
+        // Hash password
+        bcrypt.hash(values.password, 10, function (err, hash) {
+            if (err) return cb(err);
+            values.password = hash;
+            cb();
+        });
+    },
+
+    /**
+     * incase there's update of password we must hash the password
+     * @param values
+     * @param cb
+     */
+    beforeUpdate: function (values, cb) {
+
+        if (values.password) { //if it contains password , update the password
+            // Hash password
+            bcrypt.hash(values.password, 10, function (err, hash) {
+                if (err) return cb(err);
+                values.password = hash;
+                cb();
+            });
+        } else
+            cb();
+
     }
 };
 
