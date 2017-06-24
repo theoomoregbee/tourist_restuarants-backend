fdcb04f5868772a3fc74d2a66f4947c2b6ef4cb5
Theophilus Omorebee <theo4u@ymail.com>
2017-06-24 01:52:27 +0100

    policy updated correctly

--- api/policies/isAuth.js
+++ api/policies/isAuth.js
@@ -8,42 +8,44 @@
 var jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  var bearerToken;
-  var bearerHeader = req.headers["authorization"];
-  if (typeof bearerHeader !== 'undefined') {
-    var bearer = bearerHeader.split(" ");
-    bearerToken = bearer[1];
+    var bearerToken;
+    var bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        var bearer = bearerHeader.split(" ");
+        bearerToken = bearer[1];
 
-    if (bearer[0] !== Config.BEARER) {
-      return res.forbidden("bearer not understood");
+        if (bearer[0] !== Config.BEARER) {
+            return res.forbidden("bearer not understood");
+        }
+
+
+        //verify if this token was from us or not
+        jwt.verify(bearerToken, Config.SECRET_KEY, function (err, decoded) {
+            if (err) {
+                sails.log("verification error", err);
+                if (err.name === "TokenExpiredError")
+                    return res.forbidden("Session timed out, please login again");
+                else
+                    return res.forbidden("Error authenticating, please login again");
+            }
+
+
+            User.findOne(decoded.id).exec(function callback(error, user) {
+                if (error) return res.serverError(err);
+
+                if (!user) return res.serverError("User not found");
+
+                //check if the user last logged-in is greater than when the token was given
+                if (new Date(decoded.token_gen_date) < new Date(user.last_logout))
+                    return res.forbidden("Please login again, token is not valid");
+
+                req.user = user;
+                next();
+            });
+
+        });
+
+    } else {
+        return res.forbidden("No token provided");
     }
-
-
-    //verify if this token was from us or not
-    jwt.verify(bearerToken, Config.SECRET_KEY, function (err, decoded) {
-      if (err) {
-        sails.log("verification error", err);
-        if (err.name === "TokenExpiredError")
-          return res.forbidden("Session timed out, please login again");
-        else
-          return res.forbidden("Error authenticating, please login again");
-      }
-
-
-      User.findOne(decoded.id).exec(function callback(error, user) {
-        if (error) return res.serverError(err);
-
-        if (!user) return res.serverError("User not found");
-
-        if (!user.access) return res.serverError("Access denied, please contact the ict admin.");
-
-        req.user = user;
-        next();
-      });
-
-    });
-
-  } else {
-    return res.forbidden("No token provided");
-  }
 };
